59922af759ad88c8ffd1717675cd8b571ab070a2
zhazhjie <583714409@qq.com>
2020-04-23 15:22:57 +0800

    +1

--- src/components/payBox.vue
+++ src/components/payBox.vue
@@ -20,7 +20,7 @@
             <p class="gray font12"><i class="icon-common">&#xe60d;</i> 支付宝安全支付</p>
           </div>
         </label>
-        <slot></slot>
+        <slot name="other"></slot>
       </div>
       <f-button size="big" :loading="loading" @click="submitPay">确认</f-button>
     </f-popup>
