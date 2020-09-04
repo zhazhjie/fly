e63b6c2bcde09bdf8f593a1408a42beb90dd4f03
zzhijie <583714409@qq.com>
2020-09-04 21:18:47 +0800

    feat:popup添加左右位置弹出

--- src/components/popup/popup.vue
+++ src/components/popup/popup.vue
@@ -9,7 +9,8 @@
   <transition name='fade'>
     <div class="popup-wrapper" :style="{zIndex}" v-show='showFlag' :data-closable='true' @click='emitClose'
          v-if="showFlag||initFlag">
-      <div class="popup" :class='[position]' :style="{maxHeight: maxHeight}">
+      <div class="popup" :class='[position]'
+           :style="{maxHeight: position==='middle'||position==='bottom'?maxHeight:null}">
         <div class="popup-title">
           <div v-if="title" v-html="title"></div>
           <slot v-else name='title'>提示</slot>
@@ -31,103 +32,103 @@
 </template>
 
 <script>
-  import fButton from '../button';
+import fButton from '../button';
 
-  export default {
-    name: 'f-popup',
-    props: {
-      showFlag: {
-        type: Boolean,
-        default: false
-      },
-      loading: {
-        type: Boolean,
-        default: false
-      },
-      position: {
-        type: String,
-        default: 'middle'
-      },
-      title: {
-        type: String,
-        default: ""
-      },
-      content: {
-        type: String,
-        default: ""
-      },
-      simple: {
-        type: Boolean,
-        default: false
-      },
-      zIndex: {
-        type: [Number, String],
-        default: 99
-      },
-      closeOnClickModal: {
-        type: Boolean,
-        default: true
-      },
-      maxHeight: {
-        type: String,
-        default: "90vh"
-      },
-      appendToBody: {
-        type: Boolean,
-        default: false
-      }
+export default {
+  name: 'f-popup',
+  props: {
+    showFlag: {
+      type: Boolean,
+      default: false
     },
-    data() {
-      return {
-        initFlag: false
-      }
+    loading: {
+      type: Boolean,
+      default: false
     },
-    components: {fButton},
-    watch: {
-      showFlag(val) {
-        this.setBodyScroll(!val);
-        if (val && !this.initFlag) {
-          this.initFlag = true;
-        }
-      }
+    position: {
+      type: String,
+      default: 'middle'
     },
-    methods: {
-      emitClose(e) {
-        if (e.target.dataset.closable && this.closeOnClickModal) {
-          this.handleClose();
-        }
-      },
-      handleClose() {
-        this.$emit('update:showFlag', false)
-      },
-      handleSubmit() {
-        //this.handleClose();
-        this.$emit('submit');
-      },
-      setBodyScroll(flag) {
-        // document.body.style.overflow = flag ? "auto" : "hidden";
-      },
-      init() {
-        if (this.appendToBody) {
-          document.body.appendChild(this.$el);
-        }
-      },
-      clear() {
-        if (this.appendToBody) {
-          document.body.removeChild(this.$el);
-        }
-      }
+    title: {
+      type: String,
+      default: ""
     },
-    computed: {},
-    mounted() {
-      this.init();
+    content: {
+      type: String,
+      default: ""
     },
-    beforeDestroy() {
-      this.clear();
+    simple: {
+      type: Boolean,
+      default: false
+    },
+    zIndex: {
+      type: [Number, String],
+      default: 99
+    },
+    closeOnClickModal: {
+      type: Boolean,
+      default: true
+    },
+    maxHeight: {
+      type: String,
+      default: "90vh"
+    },
+    appendToBody: {
+      type: Boolean,
+      default: false
     }
+  },
+  data() {
+    return {
+      initFlag: false
+    }
+  },
+  components: {fButton},
+  watch: {
+    showFlag(val) {
+      this.setBodyScroll(!val);
+      if (val && !this.initFlag) {
+        this.initFlag = true;
+      }
+    }
+  },
+  methods: {
+    emitClose(e) {
+      if (e.target.dataset.closable && this.closeOnClickModal) {
+        this.handleClose();
+      }
+    },
+    handleClose() {
+      this.$emit('update:showFlag', false)
+    },
+    handleSubmit() {
+      //this.handleClose();
+      this.$emit('submit');
+    },
+    setBodyScroll(flag) {
+      // document.body.style.overflow = flag ? "auto" : "hidden";
+    },
+    init() {
+      if (this.appendToBody) {
+        document.body.appendChild(this.$el);
+      }
+    },
+    clear() {
+      if (this.appendToBody) {
+        document.body.removeChild(this.$el);
+      }
+    }
+  },
+  computed: {},
+  mounted() {
+    this.init();
+  },
+  beforeDestroy() {
+    this.clear();
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  @import "../../style/popup";
+@import "../../style/popup";
 </style>
